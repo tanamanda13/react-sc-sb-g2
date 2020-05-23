a097fd2cdac42600ab0f7302bdbb7b932cbe9fed
Tan <tanamanda13@gmail.com>
2020-05-23 21:06:23 +0200

    undo

--- src/stories/3-StreamerName.stories.js
+++ src/stories/3-StreamerName.stories.js
@@ -5,7 +5,7 @@ import Fonts from '../ui/particles/Fonts';
 import { withKnobs, text } from "@storybook/addon-knobs";
 
 export default {
-  title: 'Streamer Name',
+  title: 'StreamerName',
   parameters: {
     componentSubtitle: "Streamer's name display in streamer profil",
     backgrounds: [
